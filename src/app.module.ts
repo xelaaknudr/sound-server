9e3b5c310fe2331d746a523a647fefb3b3472b67
xelaaknudr <a.n.andrukovich@gmail.com>
2022-02-08 21:08:15 +0300

    te

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,10 @@ import { FileModule } from './file/file.module';
 import * as path from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
-console.log()
+console.log('232424242');
+console.log('123123123131');
+console.log('232342423');
+
 @Module({
   imports: [
     ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
